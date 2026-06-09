efe0f90d4b6f98f5fec8b64a1b86bcc7d44052d2
umbrellaknight7-sketch <umbrellaknight7@gmail.com>
2026-06-09 16:23:43 +0800

    Modify training config for LIBERO

--- examples/LIBERO/train_files/run_libero_train.sh
+++ examples/LIBERO/train_files/run_libero_train.sh
@@ -1,28 +1,35 @@
 
+# Use only GPU 0 and GPU 1.
+export CUDA_VISIBLE_DEVICES=0,1
+export NUM_PROCESSES=2
 
-export NCCL_SOCKET_IFNAME=bond0
-export NCCL_IB_HCA=mlx5_2,mlx5_3
+# Do not force a possibly wrong NIC / IB device.
+unset NCCL_SOCKET_IFNAME
+unset NCCL_IB_HCA
 
-# used for check save when communication
-export NCCL_BLOCKING_WAIT=1
-export NCCL_ASYNC_ERROR_HANDLING=1
-export NCCL_TIMEOUT=10000  # timeout set to 1 hour (unit: seconds)
-export NCCL_SOCKET_TIMEOUT_MS=360000
+# Disable InfiniBand/RoCE for debugging.
+# NCCL will fall back to IP sockets.
+export NCCL_IB_DISABLE=1
+
+# NCCL / PyTorch distributed debug settings.
+export NCCL_DEBUG=INFO
+export TORCH_NCCL_BLOCKING_WAIT=1
+export TORCH_NCCL_ASYNC_ERROR_HANDLING=1
 ###########################################################################################
 # === Please modify the following paths according to your environment ===
-Framework_name=QwenPI
-freeze_module_list=''
-base_vlm=playground/Pretrained_models/Qwen3.5-0.8B
-config_yaml=./examples/LIBERO/train_files/starvla_cotrain_libero.yaml
-libero_data_root=playground/Datasets/LEROBOT_LIBERO_DATA
+Framework_name=QwenGR00T
+freeze_module_list='qwen_vl_interface'
+base_vlm=/data/checkpoints_rui/Qwen3-VL-4B-Instruct
+config_yaml=/home/xurui/starVLA/examples/LIBERO/train_files/starvla_cotrain_libero.yaml
+libero_data_root=/data/dataset/libero
 data_mix=libero_all
-run_root_dir=./playground/Checkpoints
-run_id=1229_libero4in1_qwen3oft
+run_root_dir=/data/checkpoints_rui/starVLA_results
+run_id=1229_libero3vl4B_qwen3gr00t
 # === End of environment variable configuration ===
 ###########################################################################################
 
 
-# export WANDB_MODE=disabled
+export WANDB_MODE=disabled
 
 output_dir=${run_root_dir}/${run_id}
 mkdir -p ${output_dir}
@@ -30,11 +37,11 @@ mkdir -p ${output_dir}
 cp $0 ${output_dir}/
 
 
-num_processes=${NUM_PROCESSES:-$(nvidia-smi -L | wc -l)}
+#num_processes=${NUM_PROCESSES:-$(nvidia-smi -L | wc -l)}
 
 accelerate launch \
   --config_file starVLA/config/deepseeds/deepspeed_zero2.yaml \
-  --num_processes ${num_processes} \
+  --num_processes 2 \
   starVLA/training/train_starvla.py \
   --config_yaml ${config_yaml} \
   --framework.name ${Framework_name} \
@@ -44,14 +51,14 @@ accelerate launch \
   --datasets.vla_data.per_device_batch_size 16 \
   --trainer.vla_data.video_backend torchvision_av \
   --trainer.freeze_modules ${freeze_module_list} \
-  --trainer.max_train_steps 80000 \
-  --trainer.save_interval 10000 \
+  --trainer.max_train_steps 35000 \
+  --trainer.save_interval 5000 \
   --trainer.logging_frequency 100 \
-  --trainer.eval_interval 100 \
+  --trainer.eval_interval 1000 \
   --run_root_dir ${run_root_dir} \
   --run_id ${run_id} \
-  --wandb_project starVLA_Libero \
-  --wandb_entity jinhuiye \
+  #--wandb_project starVLA_Libero \
+  #--wandb_entity jinhuiye \
   # --is_debug True
 
 
--- examples/calvin/eval_files/eval_calvin.sh
+++ examples/calvin/eval_files/eval_calvin.sh
@@ -3,13 +3,13 @@
 ###########################################################################################
 # === Please modify the following paths according to your environment ===
 export PYTHONPATH=$(pwd):${PYTHONPATH} # let Calvin client find websocket tools from main repo
-export calvin_python=/path/to/your/conda/envs/calvin/bin/python
+export calvin_python=/home/xurui/miniconda3/envs/calvin_venv/bin/python
 
 host="127.0.0.1"
 base_port=5694
-your_ckpt=results/Checkpoints/0118_starvla_qwengr00t_calvin_task_D_D/checkpoints/steps_30000_pytorch_model.pt
-dataset_path=/path/to/calvin/task_D_D/
-calvin_config_path=/path/to/calvin/calvin_models/conf
+your_ckpt=results/Checkpoints/0118_starvla_qwengr00t_calvin_task_D_D_v3.0/checkpoints/steps_30000_pytorch_model.pt
+dataset_path=/data/dataset/calvin/calvin_task_D_D_v3.0
+calvin_config_path=/data/dataset/calvin/calvin_repo/calvin_models/conf
 eval_sequences_path=examples/calvin/eval_files/eval_sequences.json
 
 folder_name=$(echo "$your_ckpt" | awk -F'/' '{print $(NF-2)"_"$(NF-1)"_"$NF}')
